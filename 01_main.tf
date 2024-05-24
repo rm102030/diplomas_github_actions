9a14bedb4f37b9b67bc2746f7d19cb9f940f7af9
Ricardo Martinez <ricardo.martinez@RICARDOMARTINEZ-2.local>
2024-05-24 14:26:46 -0500

    update provider3

--- 01_main.tf
+++ 01_main.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = "0.14.3"
-  #required_version = "1.4.6"
+  #required_version = "0.14.3"
+  required_version = "1.4.6"
 }
 provider "aws" {
   region  = var.aws_region
-  #profile = var.aws_profile
+  profile = var.aws_profile
 }
 
 
